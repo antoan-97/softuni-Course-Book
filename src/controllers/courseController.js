e24da0eb81ba4b6336d434f1c0e32ff83c3faa2b
antoan-97 <antoan.todorov@abv.bg>
2024-05-28 17:18:29 +0300

    render create page

--- src/controllers/courseController.js
+++ src/controllers/courseController.js
@@ -1,2 +1,8 @@
 const router = require('express').Router();
 
+
+router.get('/create', (req,res) =>{
+  res.render('courses/create');
+});
+
+module.exports = router;
--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,12 @@
 const router = require('express').Router();
 const homeController = require('./controllers/homeController');
 const userController = require('./controllers/userController');
-
+const courseController = require('./controllers/courseController');
 
 
 router.use(homeController);
 router.use('/users',userController);
+router.use('/courses',courseController);
 
 
 module.exports = router
