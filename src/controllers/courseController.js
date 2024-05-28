6c29005aca07aaded7c06481a2a38aee861784c4
antoan-97 <antoan.todorov@abv.bg>
2024-05-28 17:23:34 +0300

    add create controller

--- src/controllers/courseController.js
+++ src/controllers/courseController.js
@@ -5,4 +5,17 @@ router.get('/create', (req,res) =>{
   res.render('courses/create');
 });
 
+router.post('/create',async (req,res) =>{
+    const courseData = {
+        ...req.body,
+        owner:req.user._id,
+    };
+
+    try {
+        await courseManager.create(courseData);
+    } catch (error) {
+        res.render('courses/create');
+    }
+});
+
 module.exports = router;
