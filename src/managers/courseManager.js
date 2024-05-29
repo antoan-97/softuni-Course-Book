9d97058f69ebe1bd3a0c3a4a0c1a6b4782904328
antoan-97 <antoan.todorov@abv.bg>
2024-05-29 12:52:35 +0300

    create signUp manager

--- src/managers/courseManager.js
+++ src/managers/courseManager.js
@@ -3,4 +3,22 @@ const Course = require('../models/Course');
 
 exports.create = (courseData) => Course.create(courseData);
 
-exports.getAll = () =>Course.find().populate('owner');
+exports.getAll = () => Course.find().populate('owner');
+
+exports.getOne = (courseId) => Course.findById(courseId).populate('owner');
+
+exports.signUp = async (courseId, userId) => {
+    const course = await Course.findById(courseId);
+
+    if (!course) {
+        throw new Error('Stone not found');
+    }
+
+    // Check if the user has already voted
+    if (course.signUpList.includes(userId)) {
+        return;
+    }
+
+    course.signUpList.push(userId);
+    return course.save();
+};
